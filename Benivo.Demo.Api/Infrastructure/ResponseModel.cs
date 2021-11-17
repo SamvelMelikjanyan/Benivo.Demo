66a9a80489c10bba724329236058cbb43246acc2
Samvel Melikjanyan <melikjanyan10@gmail.com>
2021-11-18 01:00:55 +0400

    move response model to benivo.demo.api

--- Benivo.Demo.Api/Infrastructure/ResponseModel.cs
+++ Benivo.Demo.Api/Infrastructure/ResponseModel.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
-namespace Benivo.Demo.ApiModels.Infrastructure
+namespace Benivo.Demo.Api.Infrastructure
 {
     public class ResponseModel<TData> : OkResult
     {
