54b95e04fa56ace79d5e1782a6fa759452fbd42f
Samvel Melikjanyan <melikjanyan10@gmail.com>
2021-11-19 18:36:23 +0400

    get employment types

---
+++ Benivo.Demo.Api/Controllers/EmploymentTypeController.cs
@@ -0,0 +1,23 @@
+﻿using Benivo.Demo.Api.Constants;
+using Benivo.Demo.Api.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Benivo.Demo.Api.Controllers
+{
+    [ApiVersion(ApiVersioning.ApiVersion1)]
+    public class EmploymentTypeController : BaseController
+    {
+        public EmploymentTypeController(ServiceFactory serviceFactory) : base(serviceFactory)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await ServiceFactory.EmploymentTypeService.GetAllAsync();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Benivo.Demo.BLL/Services/EmploymentTypeService.cs
@@ -0,0 +1,33 @@
+﻿using Benivo.Demo.BLL.Infrastructure;
+using Benivo.Demo.BLL.Interfaces.Services;
+using Benivo.Demo.Cache;
+using Benivo.Demo.DAL.Infrastructure;
+using Benivo.Demo.Models.Outputs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Benivo.Demo.BLL.Services
+{
+    public class EmploymentTypeService : Service, IEmploymentTypeService
+    {
+        public EmploymentTypeService(BenivoDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<GetEmploymentTypeOutput>> GetAllAsync()
+        {
+            var employmentTypes = await CacheHelper.GetOrSetAsync(CacheKeys.EmploymentTypes,
+                async () => await DbContext.EmploymentTypes.AsNoTracking().ToListAsync(),
+                TimeSpan.FromHours(1));
+
+            return employmentTypes.Select(et => new GetEmploymentTypeOutput
+            {
+                Id = et.Id,
+                Name = et.Name
+            }).ToList();
+        }
+    }
+}
