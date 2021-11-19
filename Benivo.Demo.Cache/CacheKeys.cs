d231a754a0f459a2ae2965f7b947b3579f1182c3
Samvel Melikjanyan <melikjanyan10@gmail.com>
2021-11-19 18:38:18 +0400

    add cache keys

--- Benivo.Demo.Cache/CacheKeys.cs
+++ Benivo.Demo.Cache/CacheKeys.cs
@@ -3,6 +3,8 @@
     public static class CacheKeys
     {
         public const string JobCategories = "JobCategories";
+        public const string EmploymentTypes = "EmploymentTypes";
+        public const string Locations = "Locations";
         public const string BenivoIdentityInfo = "BenivoIdentityInfo";
     }
 }
