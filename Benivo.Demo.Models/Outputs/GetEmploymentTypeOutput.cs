576653003c69604d4a61e91ab8cf7d08e43252e7
Samvel Melikjanyan <melikjanyan10@gmail.com>
2021-11-19 18:37:04 +0400

    get employment types

---
+++ Benivo.Demo.Models/Outputs/GetEmploymentTypeOutput.cs
@@ -0,0 +1,11 @@
+﻿using Benivo.Demo.Models.Infrastructure;
+
+namespace Benivo.Demo.Models.Outputs
+{
+    public class GetEmploymentTypeOutput : BaseOutput
+    {
+        public byte Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
